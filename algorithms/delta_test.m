24d01ed9f26aa93a76e8562ac1e72ed5e3f3effc
Yury Brodskiy <yurashkaus@gmail.com>
2015-07-29 16:35:24 +0200

    update octave scrips

--- algorithms/delta_test.m
+++ algorithms/delta_test.m
@@ -2,7 +2,13 @@ robot.end_effector = 18;
 robot.base = 40;            
 robot.bicep_length = 90;
 robot.forearm_length = 130;
-theta1=45;
-theta2=45;
-theta3=45;
-result = delta_calcForward(robot, theta1, theta2, theta3) 
+robot.servo_step = 0.5;
+
+theta1=25;
+theta2=25;
+theta3=25;
+H = delta_calcForward(robot, theta1, theta2, theta3) 
+H = delta_calcForward(robot, theta1-1, theta2+1, theta3+1) 
+H = delta_calcForward(robot, theta1+1, theta2-1, theta3+1) 
+H = delta_calcForward(robot, theta1+1, theta2+1, theta3-1) 
+%q = delta_calcInverse(robot, H.x,H.y, H.z)
